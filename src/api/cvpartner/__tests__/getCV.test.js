02277ce22bcfcf9a654bf110412a633a6c8b746d
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2018-09-05 14:46:10 +0200

    Add test and test data to ensure mapping of cv is according to snapshot

--- src/api/cvpartner/__tests__/getCV.test.js
+++ src/api/cvpartner/__tests__/getCV.test.js
@@ -1,5 +1,6 @@
 import getCV from '../getCV';
 import getRequest from '../getRequest';
+import testData from './__data__/cv.json'
 
 const userId = 'idUser';
 const cvId = 'idCv';
@@ -16,5 +17,13 @@ describe('getCV', () => {
     it('should use correct url', async () => {
         await getCV(user);
         expect(getRequest).toHaveBeenCalledWith(`/api/v3/cvs/${userId}/${cvId}`);
+    });
+    it('should reject errors', async () => {
+        getRequest.mockImplementationOnce(() => Promise.reject({ error: 'error' }));
+        await expect(getCV(user)).rejects.toMatchSnapshot();
+    });
+    it('should map according to snapshot', async () => {
+        getRequest.mockImplementationOnce(() => Promise.resolve(testData));
+        await expect(getCV(user)).resolves.toMatchSnapshot();
     });
 });
