c47b95a18079aeb7f8b9b3c0f9d0eed67c17eaf5
Fredrik Oterholt <fredrik.oterholt@solidsquare.no>
2018-09-17 15:11:05 +0200

    Fix storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,16 +1,26 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
 import jest from 'jest-mock';
-
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
 import '../src/components/Ui/Theme';
+import { getMessages } from '../src/translation';
 
 
 window.jest = jest;
-window.describe = () => {};
-window.it = () => {};
+window.describe = () => {
+};
+window.it = () => {
+};
+
+setIntlConfig({
+  locales: ['en', 'nb'],
+  defaultLocale: 'en',
+  getMessages,
+});
+addDecorator(withIntl);
 
 function loadStories() {
-    require('../stories/Ui.stories.jsx');
-    require('../stories/SalaryParameters.stories');
+  require('../stories/Ui.stories.jsx');
+  require('../stories/SalaryParameters.stories');
 }
 
 configure(loadStories, module);
--- stories/Ui.stories.jsx
+++ stories/Ui.stories.jsx
@@ -1,9 +1,11 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import withReduxForm from 'redux-form-storybook';
 import { Checkbox, InputWithLabel, Section } from '../src/components/Ui';
 
 
 storiesOf('Ui', module)
+  .addDecorator(withReduxForm)
   .add('Section', () => <Section header="The title">child</Section>)
   .add('InputWithLabel', () => <InputWithLabel value={123} name="in" label="Value" />)
   .add('Checkbox', () => <Checkbox name="ch" label="Value" />);
